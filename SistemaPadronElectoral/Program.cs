9867a4e667eb2b375100d75d3c05d78615540893
camila-luksic <83142600+camila-luksic@users.noreply.github.com>
2025-06-20 11:44:23 -0400

    Update CORS Program.cs

--- SistemaPadronElectoral/Program.cs
+++ SistemaPadronElectoral/Program.cs
@@ -6,6 +6,7 @@ using System.IO;
 
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 builder.Services.AddDbContext<SistemaPadronElectoralContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SistemaPadronElectoralContext") ?? throw new InvalidOperationException("Connection string 'SistemaPadronElectoralContext' not found.")));
 
@@ -15,6 +16,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:5173") 
+                                 .AllowAnyHeader()      
+                                 .AllowAnyMethod() 
+                                 .AllowCredentials();
+                      });
+});
 
 var app = builder.Build();
 
@@ -26,7 +39,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
 
 var uploadsPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
